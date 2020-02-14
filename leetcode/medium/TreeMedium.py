862709a826ace217f52d02e04571f3c939e6da5b
benoitantelme <benoitantelme@gmail.com>
2020-02-14 18:07:06 +0000

    leetcode mediums tree bfs/dfs exercises

--- leetcode/medium/TreeMedium.py
+++ leetcode/medium/TreeMedium.py
@@ -17,7 +17,6 @@ def bfs_print(root: TreeNode):
             queue.append(current.right)
 
 
-
 def deepestLeavesSum(root: TreeNode) -> int:
     sum = []
     stack = [(root, 1)]
@@ -26,15 +25,13 @@ def deepestLeavesSum(root: TreeNode) -> int:
         if len(sum) < depth:
             sum.append(current.val)
         else:
-            sum[depth-1] += current.val
+            sum[depth - 1] += current.val
         if current.left:
-            stack.append((current.left, depth+1))
+            stack.append((current.left, depth + 1))
         if current.right:
-            stack.append((current.right, depth+1))
+            stack.append((current.right, depth + 1))
 
-    return sum[len(sum)-1]
-
-
+    return sum[len(sum) - 1]
 
 
 root = TreeNode(1)
@@ -77,5 +74,43 @@ root.left.left.left = TreeNode(9)
 root.left.right.left = TreeNode(1)
 root.left.right.right = TreeNode(4)
 root.right.right.right = TreeNode(5)
+print('Sum grand parents: ')
 print(sumEvenGrandparent(root))
 
+def in_order_dfs_rewrite(current: TreeNode, total: int) -> int:
+    if current.left:
+        total = in_order_dfs_rewrite(current.left, total)
+    tmp = current.val
+    current.val = total
+    total = total - tmp
+    if current.right:
+        total = in_order_dfs_rewrite(current.right, total)
+    return total
+
+def bstToGst(root: TreeNode) -> TreeNode:
+    # get total with bfs
+    total = 0
+    queue = [root]
+    while len(queue) > 0:
+        current = queue.pop(0)
+        total += current.val
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+
+    in_order_dfs_rewrite(root, total)
+    return root
+
+
+root = TreeNode(4)
+root.left = TreeNode(1)
+root.right = TreeNode(6)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(2)
+root.right.left = TreeNode(5)
+root.right.right = TreeNode(7)
+root.left.right.right = TreeNode(3)
+root.right.right.right = TreeNode(8)
+print('Values modified:')
+bfs_print(bstToGst(root))
