9fbb4320d27985d5f0b530bf8a92fcdc482eef71
benoitantelme <benoitantelme@gmail.com>
2020-02-16 22:58:54 +0000

    leetcode mediums array exercises

--- leetcode/medium/ArrayMedium.py
+++ leetcode/medium/ArrayMedium.py
@@ -1,3 +1,4 @@
+import collections
 from collections import defaultdict
 from typing import List
 
@@ -8,7 +9,7 @@ def diagonalSort(mat: List[List[int]]) -> List[List[int]]:
     tmp = defaultdict(list)
 
     # get diagonals on y
-    for y in range(m-1, -1, -1):
+    for y in range(m - 1, -1, -1):
         x = 0
         start = (x, y)
         while m > y >= 0 and n > x >= 0:
@@ -43,3 +44,18 @@ def diagonalSort(mat: List[List[int]]) -> List[List[int]]:
 
 
 print(diagonalSort([[3, 3, 1, 1], [2, 2, 1, 2], [1, 1, 1, 2]]))
+
+
+def deckRevealedIncreasing(deck: List[int]) -> List[int]:
+    res = collections.deque()
+    for card in sorted(deck, reverse=True):
+        res.rotate()
+        res.appendleft(card)
+    return list(res)
+
+
+print(deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7]))
+print(deckRevealedIncreasing([1]))
+print(deckRevealedIncreasing([1, 3]))
+print(deckRevealedIncreasing([1, 3, 2, 4]))
+print(deckRevealedIncreasing([1, 2, 3, 4, 5]))
