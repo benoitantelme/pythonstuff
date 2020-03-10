52bdb67b740d571ad544787311e763f6a8679001
benoitantelme <benoitantelme@gmail.com>
2020-03-10 18:59:54 +0000

    leetcode medium graph union find exercise

--- leetcode/medium/GraphMedium.py
+++ leetcode/medium/GraphMedium.py
@@ -24,7 +24,27 @@ class Solution:
 
         return self.result
 
+    def find(self, parents: List[int], i: int) -> int:
+        if parents[i] == -1:
+            return i
+        else:
+            return self.find(parents, parents[i])
+
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parents = [-1] * (len(set([item for edge in edges for item in edge])) + 1)
+        for edge in edges:
+            x = self.find(parents, edge[0])
+            y = self.find(parents, edge[1])
+
+            # loop
+            if x == y:
+                return edge
+            parents[x] = y
+        return None
+
 
 s = Solution()
 # print(s.allPathsSourceTarget([[1, 2], [3], [3], []]))
 print(s.allPathsSourceTarget([[4, 3, 1], [3, 2, 4], [3], [4], []]))
+print(s.findRedundantConnection([[1, 2], [1, 3], [2, 3]]))
+print(s.findRedundantConnection([[1, 2], [2, 3], [3, 4], [1, 4], [1, 5]]))
