d02dfbc1840fd53023ea8f4e14e3a0176c5915ae
benoitantelme <benoitantelme@gmail.com>
2020-04-11 16:06:31 +0100

    leetcode medium Counting bits better/worse solutions

--- leetcode/medium/BitsMedium.py
+++ leetcode/medium/BitsMedium.py
@@ -3,11 +3,22 @@ from typing import List
 
 class Solution:
 
-    def countBits(self, num: int) -> List[int]:
+    def countBits2(self, num: int) -> List[int]:
         result = []
         for i in range(0, num+1):
-            result.append(len([x for x in bin(i) if x == '1']))
+            result.append(bin(i).count('1'))
         len([x for x in bin(num) if x == '1'])
+        return result
+
+    def countBits(self, num: int) -> List[int]:
+        # endup being slower ....
+        result = []
+        for i in range(0, num+1):
+            y = 0
+            while i:
+                y += i & 1
+                i = i >> 1
+            result.append(y)
         return result
 
 
