f30e8d4174a1cb3e1d7eadff958a2f8f66c3867c
benoitantelme <benoitantelme@gmail.com>
2020-04-12 15:24:08 +0100

    leetcode medium lowest common ancestor on trees

--- leetcode/medium/Tree3Medium.py
+++ leetcode/medium/Tree3Medium.py
@@ -5,6 +5,9 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return 'node:' + str(self.val)
+
 
 class FindElements:
 
@@ -34,3 +37,51 @@ root.right = TreeNode(-1)
 f = FindElements(root)
 print(f.find(1))
 print(f.find(2))
+
+
+class LowestCommonAncestor:
+    def height(self, node: TreeNode) -> int:
+        if not node:
+            return 0
+
+        l = self.height(node.left)
+        r = self.height(node.right)
+        return max(l, r) + 1
+
+    def recPostOrder(self, node: TreeNode, depth: int, height: int) -> TreeNode:
+        if node is None:
+            return None
+
+        if node.left is None and node.right is None and depth == height:
+            return node
+
+        left = self.recPostOrder(node.left, depth + 1, height)
+        right = self.recPostOrder(node.right, depth + 1, height)
+
+        if left is None:
+            return right
+        if right is None:
+            return left
+        return node
+
+
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        height = self.height(root)
+        return self.recPostOrder(root, 1, height)
+
+
+lca = LowestCommonAncestor()
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+res = lca.lcaDeepestLeaves(root)
+print(res)
+
+root.left.left = TreeNode(4)
+res = lca.lcaDeepestLeaves(root)
+print(res)
+
+root.left.right = TreeNode(5)
+res = lca.lcaDeepestLeaves(root)
+print(res)
